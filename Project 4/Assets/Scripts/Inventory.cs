46d0e58290225c255da7edb742b164ed8143b06f
Leigh <johnstonlh@hendrix.edu>
2025-03-06 11:49:59 -0600

    Made script for throwable items
    Base for throwing added, figuring out items

---
+++ Project 4/Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+     List<Item> inventory = new List<Item>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddItem(Item itemName){
+        inventory.Add(itemName);
+        Debug.Log("adding: " + itemName);
+
+    }
+
+    public void RemoveItem(Item itemName){
+        inventory.Remove(itemName);
+
+    }
+
+    public bool HasItem(Item item){
+        if(inventory.Contains(item)){
+            return true;
+            }
+        return false;
+    }
+}
---
+++ Project 4/Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    private Rigidbody itemBody;
+    public GameObject goal;
+
+    public bool isHeld = false;
+
+    private bool isIn = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        itemBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(itemBody.position.x - goal.transform.position.x) <= 1 && Mathf.Abs(itemBody.position.y - goal.transform.position.y) <= 1 && Mathf.Abs(itemBody.position.z - goal.transform.position.z) <= 1){
+            isIn = true;
+        }
+       
+    }
+
+
+
+//not sure how to structure this part but it's not my job lol
+    private void Throw(){
+        if(isHeld && !isIn){
+            itemBody.AddForce(new Vector3(5,0,0), ForceMode.Force);
+            isHeld = false;
+        }
+    }
+
+    
+    
+}
