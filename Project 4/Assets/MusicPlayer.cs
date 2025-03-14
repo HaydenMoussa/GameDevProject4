f62b00295dbe73397d46264c117d8488fd0bda6d
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-03-13 20:53:07 -0500

    Added sfx

--- Project 4/Assets/MusicPlayer.cs
+++ Project 4/Assets/MusicPlayer.cs
@@ -5,11 +5,27 @@ public class MusicPlayer : MonoBehaviour
     GameObject self;
     AudioSource audio;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                self = gameObject;
+        DontDestroyOnLoad(self);
+        audio = self.GetComponent<AudioSource>();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    public static MusicPlayer Instance { get; private set; }
+    
     void Start()
     {
-    self = gameObject;
-    DontDestroyOnLoad(self);
-    audio = self.GetComponent<AudioSource>();        
+            
     }
 
     // Update is called once per frame
